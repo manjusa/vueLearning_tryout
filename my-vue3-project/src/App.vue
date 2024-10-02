152b01733dc12c75c5b282bd01eb0b973129eb93
manjusa <thizismanju@mail.com>
2024-10-03 08:23:38 +1000

    added to git ignore and basic todolist

---
+++ my-vue3-project/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { onMounted } from "vue";
+
+import TodoList from "./components/TodoList.vue";
+
+// lifecycle hooks
+onMounted(() => {
+  console.log(`App vue is mounted`);
+});
+</script>
+
+<template>
+  <TodoList />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-vue3-project/src/components/TodoList.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from "vue";
+const task = ref();
+</script>
+
+<template>
+  <form>
+    <input type="text" name="task" v-model="task" />
+
+    {{ task }}
+    //now emit event from here to show on parent
+  </form>
+</template>
